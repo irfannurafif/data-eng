b961428d51936662dafe8128437f0f9e952e1b8c
Johannes Altmanninger <aclopte@gmail.com>
2017-06-18 11:08:10 +0200

    update

--- encypher.py
+++ encypher.py
@@ -3,6 +3,7 @@
 import csv
 import re
 import itertools
+from datetime import datetime
 
 # pip install neo4j-driver
 from neo4j.v1 import GraphDatabase, basic_auth
@@ -41,12 +42,10 @@ def edge(edgelabel, edgedict, srclabel, srcdict, dstlabel, dstdict):
 
 csvfile = open('Camden_Licensing_Applications_Beta_filtered.csv')
 reader = csv.DictReader(csvfile)
-# license_application_data = itertools.islice(reader, 500)
-license_application_data = [row for row in reader]
+licence_application_data = [row for row in reader]
 
 csvfile = open('Companies_Registered_In_Camden_And_Surrounding_Boroughs_filtered.csv')
 reader = csv.DictReader(csvfile)
-# company_data = itertools.islice(reader, 500)
 company_data = [row for row in reader]
 
 nodeattrs = {
@@ -68,15 +67,32 @@ nodeedges = [
     ('premises', 'has_applied_at', [], 'ward'),
 ]
 
-session.run('MATCH (n) DETACH DELETE n')
+RECREATE = 1
+# canonicalize date (so it compared lexicographically)
+def cd(d):
+    if not d:
+        return ''
+    dt = datetime.strptime(d, '%d/%m/%Y')
+    return datetime.strftime(dt, '%Y/%m/%d')
 
-if 1:
+if 0:
+    #
+    for row in licence_application_data:
+        dd = (row['Approved Date'], row['Expiry Date'], row['Hearing Date'],
+              row['Date Of Events'], row['Received Date'], row['Refused Date'])
+        assert(len(d) == 10 for d in dd)
+        print("%11s _%11s _%11s _%11s _%11s _%11s" % dd)
+
+if RECREATE:
+    session.run('MATCH (n) DETACH DELETE n')
+
+if RECREATE:
     # create the 3 status nodes
     node('application_status', {'status': 'open'})
     node('application_status', {'status': 'approved'})
     node('application_status', {'status': 'refused'})
 
-    for row in license_application_data:
+    for row in licence_application_data:
         for nodelabel in nodeattrs:
             attrs = nodeattrs[nodelabel]
             node(nodelabel, kvs(row, attrs))
@@ -86,13 +102,13 @@ if 1:
             edge(edgelabel, kvs(row, edgeattrs),
                  srclabel, kvs(row, srcattrs),
                  dstlabel, kvs(row, dstattrs))
-        d_approved = row['Approved Date']
-        d_expiry = row['Expiry Date']
-        d_hearing = row['Hearing Date']
-        d_events = row['Date Of Events']
-        d_last = row['Last Date For Representations'] # TODO what is this
-        d_received = row['Received Date']
-        d_refused = row['Refused Date']
+        d_approved = cd(row['Approved Date'])
+        d_expiry = cd(row['Expiry Date'])
+        d_hearing = cd(row['Hearing Date'])
+        d_events = cd(row['Date Of Events'])
+        d_last = cd(row['Last Date For Representations']) # TODO what is this
+        d_received = cd(row['Received Date'])
+        d_refused = cd(row['Refused Date'])
         # print('received %10s, refused %10s, approved %10s, either %10s' %
         #       (d_received, d_refused, d_approved, d_refused or d_approved))
         if d_approved:
@@ -108,7 +124,7 @@ if 1:
                  'application', kvs(row, nodeattrs['application']),
                  'application_status', {'status': 'refused'})
 
-if 1:
+if RECREATE:
     for row in company_data:
         node('ward', kvs(row, ['Ward Code']))
         node('company', kvs(row, ['Company Name', 'Company Category']))
@@ -117,8 +133,68 @@ if 1:
              'company', kvs(row, ['Company Name', 'Company Category']),
              'ward', kvs(row, ['Ward Code']))
 
-# MATCH (c:company)-[:is_registered_at]->(w:ward), (p:premises)-[:has_applied_at]->(w)
-# RETURN c, w, p
-# LIMIT 1000;
-
 session.close()
+
+"""
+MATCH (c:company)-[:is_registered_at]->(w:ward), (p:premises)-[:has_applied_at]->(w)
+RETURN c, w, p
+LIMIT 1000;
+"""
+
+'E05000131'
+'E05000134'
+'E05000136'
+'E05000139'
+"Cantelowes"
+"Gospel Oak"
+"Haverstock"
+"Kentish Town"
+
+"""
+MATCH (a:application)-[:belongs_to]->(w:ward), (a)-[:is_a]->(t:application_type)
+WHERE w._ward_name = 'Gospel Oak'
+RETURN w, a, t
+"""
+
+"""
+MATCH (c:company)-[:is_registered_at]->(w:ward)
+WHERE w._ward_name = 'Haverstock'
+RETURN c, w
+"""
+
+"""
+MATCH (a:application)-[:belongs_to]->(w:ward), (a)-[:is_a]->(t:application_type)
+WHERE left(a._approved_date, 4) == 2008
+RETURN w, a, t
+"""
+
+"""
+MATCH (a:application)-[:belongs_to]->(w:ward), (a)-[:is_a]->(t:application_type), (c:company)-[reg:is_registered_at]->(w)
+WHERE right(a._approved_date, 4) = '2008'
+  AND reg._from <= '2008' AND (reg._until >= '2008' or reg._until = '')
+RETURN w, a, t, c
+"""
+
+"""
+MATCH (s)-[reg:is_registered_at|belongs_to]->(d)
+WHERE (s:company AND
+       reg._from <= '2008' AND (reg._until >= '2008' OR reg._until = ''))
+   OR (s:application)
+RETURN s, d
+"""
+
+"""
+MATCH (s)-[reg:is_registered_at|belongs_to]->(d)
+WHERE (s:company AND
+       right(reg._from, 4) <= '2008' AND (right(reg._until, 4) >= '2008' OR right(reg._until, 4) = ''))
+   OR (s:application)
+RETURN s, d
+"""
+
+"""
+MATCH (s)-[reg:is_registered_at|belongs_to]->(d), (s)-[:is_a|is_registered_at]->(t)
+WHERE (s:company AND right(reg._from, 4) = '2008' AND t:ward AND d:ward)
+   OR (s:application AND right(s._approved_date, 4) = '2008'
+   AND t:application_type)
+RETURN s, d, t
+"""
